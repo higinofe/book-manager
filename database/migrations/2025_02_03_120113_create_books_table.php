d332d0a3d61aab6eaac2e3bc2ab84f320b11cdf0
higinofe <fernandohigino.fsh@gmail.com>
2025-02-03 12:26:24 +0000

    created: install breeze and migrate

--- database/migrations/2025_02_03_120113_create_books_table.php
+++ database/migrations/2025_02_03_120113_create_books_table.php
@@ -13,6 +13,13 @@ return new class extends Migration
     {
         Schema::create('books', function (Blueprint $table) {
             $table->id();
+            $table->string('titulo');
+            $table->text('descricao');
+            $table->string('autor');
+            $table->decimal('preco', 8, 2);
+            $table->integer('quantidade_estoque');
+            $table->text('autor_biografia')->nullable();
+            $table->string('autor_nacionalidade')->nullable();
             $table->timestamps();
         });
     }
